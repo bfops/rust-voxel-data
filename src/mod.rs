5a3578b3eac4dd2b6736407cdc72179a185de247
bjorn3 <bjorn3@users.noreply.github.com>
2017-12-30 11:55:35 +0100

    Remove nightlyness

--- src/mod.rs
+++ src/mod.rs
@@ -3,10 +3,6 @@
 #![deny(missing_docs)]
 #![deny(warnings)]
 
-#![feature(main)]
-#![feature(test)]
-#![feature(unboxed_closures)]
-
 extern crate cgmath;
 extern crate collision;
 #[macro_use]
@@ -14,7 +10,6 @@ extern crate log;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate test;
 
 pub mod bounds;
 pub mod brush;
